54c37155c6be9ab9583a226d64f5f27ad2ba3be5
Alena Frydrychova <AlFryd@moravia.com>
2022-12-06 11:42:20 +0100

    Day 6 done ✨

---
+++ AdventOfCode2022/AdventOfCode2022/Day06.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode2022
+{
+    public class Day06
+    {
+        public void SolvePuzzle()
+        {
+            var input = File.ReadAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Inputs\Day06.txt")).ToCharArray().ToList();
+            var listOfChars = new List<char>();
+            var index = 0;
+            foreach (var character in input)
+            {
+                listOfChars.Add(character);
+                index++;
+                if (CheckIfInList(listOfChars, character))
+                {
+                    Console.WriteLine(index);
+                }
+            }
+        }
+
+        private bool CheckIfInList(List<char> listOfChars, char character)
+        {
+            var lastChars = listOfChars.Skip(Math.Max(0, listOfChars.Count() - 14));
+            return lastChars.Distinct().Count() == 14;
+        }
+    }
+}
--- AdventOfCode2022/AdventOfCode2022/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2022;
 
-var currentDay = new Day05();
+var currentDay = new Day06();
 currentDay.SolvePuzzle();
